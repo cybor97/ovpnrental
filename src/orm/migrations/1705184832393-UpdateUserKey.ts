4fd8018d664bc0a0e8b351003a02af41d7f73a63
Ruslan Mukhametshyn <ruslan@payflow.es>
2024-01-14 17:27:21 +0100

    fix: fix migration

--- src/orm/migrations/1705184832393-UpdateUserKey.ts
+++ src/orm/migrations/1705184832393-UpdateUserKey.ts
@@ -5,7 +5,7 @@ export class UpdateUserKey1705184832393 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "temporary_user_key" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "key" varchar NOT NULL, "status" varchar NOT NULL DEFAULT ('active'), "eternal" boolean NOT NULL DEFAULT (0), "userId" integer, "vpnServerId" integer, "tgMetadata" json NOT NULL, CONSTRAINT "FK_ad8166e0fec3d9c3186805bf6a4" FOREIGN KEY ("vpnServerId") REFERENCES "vpn_server" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT "FK_123efe6d100956719d638500e5e" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`
+      `CREATE TABLE "temporary_user_key" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "key" varchar NOT NULL, "status" varchar NOT NULL DEFAULT ('active'), "eternal" boolean NOT NULL DEFAULT (0), "userId" integer, "vpnServerId" integer, "tgMetadata" json NOT NULL DEFAULT '{}', CONSTRAINT "FK_ad8166e0fec3d9c3186805bf6a4" FOREIGN KEY ("vpnServerId") REFERENCES "vpn_server" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT "FK_123efe6d100956719d638500e5e" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`
     );
     await queryRunner.query(
       `INSERT INTO "temporary_user_key"("id", "key", "status", "eternal", "userId", "vpnServerId") SELECT "id", "key", "status", "eternal", "userId", "vpnServerId" FROM "user_key"`
