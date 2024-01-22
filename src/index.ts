c29cac23d7a4b355a4ffa3c4aff560cbb3ca6d70
cyborg97 <muhametshin.1997@gmail.com>
2024-01-23 00:39:29 +0100

    fix: fix ds init sequence

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import "dotenv/config";
 import { initBot } from "./entryPoints/bot";
 import { initCron } from "./entryPoints/cron";
 import { initSQS } from "./entryPoints/sqs";
+import AppDataSource from "./orm/dataSource";
 
 async function main() {
+  await AppDataSource.initialize();
   await initCron();
   await initBot();
   await initSQS();
